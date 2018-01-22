f3eeb0aca962b3a2565938f290be01fa91b00936
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-01-21 20:21:19 -0700

    adding a time check to auto stop after 1 hour to save on CPU resource utilization

--- pyradio/__init__.py
+++ pyradio/__init__.py
@@ -2,7 +2,7 @@ import subprocess
 import psutil
 import time
 import os
-
+import datetime
 
 class StationInfo:
     """
@@ -23,6 +23,7 @@ class StreamPlayer:
     def __init__(self, station_info):
         self.station = station_info
         self._is_running = False
+        self.started = None
         self.process = None
         self.pids = []
         self.pre_play_pids = []
@@ -57,6 +58,7 @@ class StreamPlayer:
 
         self.process = subprocess.Popen(cli_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         self._is_running = self.is_playing()
+        self.started = datetime.now()
         time.sleep(2)  # need to sleep for a couple of seconds as it take mplayer 1-2 seconds to spin up the 2nd process
         self.pids = [proc.pid for proc in psutil.process_iter() if proc.name() == 'mplayer' and
                      not self.pre_play_pids.__contains__(proc.pid)]
@@ -66,14 +68,21 @@ class StreamPlayer:
         # so we need to kill all mplayer processes that were opened prior to logging pre_play_pids
         for pid in self.pids:
             os.kill(pid, 15)  # SIGTERM = 16
+        self.started = None
         self._is_running = self.is_playing()
 
     def is_playing(self):
-        # If the subprocess.Popen.poll() returns a value then the process is still running
+        # If the subprocess.Popen.poll() returns a value then the process is not running
         if self.process.poll():
             self._is_running = False
         else:  # if the subprocess.Popen.poll() returns None then the process is still running
-            self._is_running = True
+            # the longer mplayer streams the more CPU resources are used. So every hour (3600
+            # seconds) lets stop mplayer.
+            time_delta = datetime.now() - self.started
+            if time_delta.total_seconds >= 3600:
+                self.stop()
+            else:
+                self._is_running = True
 
         return self._is_running
 
